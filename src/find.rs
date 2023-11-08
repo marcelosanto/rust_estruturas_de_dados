4908b579c69e58aeecdb6da4a28b34ba8d656200
Marcelo Santos <rocha.mer21@gmail.com>
2023-11-08 14:54:48 -0300

    refatorado o codigo

--- src/find.rs
+++ src/find.rs
@@ -1,28 +1,21 @@
-pub fn busca_binaria(array: &[i32], buscado: i32) -> Option<&str> {
-  
-  if array.is_empty() {
-    None
-  } else {
-    let mut inicio = 0;
-    let mut fim = array.len() - 1;
-    let mut encontrado = false;
+pub fn busca_binaria(array: &[i32], buscado: i32) -> Option<usize> {
+    if array.is_empty() {
+        None
+    } else {
+        let mut inicio = 0;
+        let mut fim = array.len() - 1;
 
-    while inicio <= fim {
-        let meio = (inicio+fim)/2;
+        while inicio <= fim {
+            let meio = (inicio + fim) / 2;
 
-        match array[meio] {
-            x if x == buscado => {
-              encontrado = !encontrado;
-              break;
-            },
-            x if x < buscado => inicio = meio + 1,
-            _ => fim = meio - 1
+            match array[meio] {
+                x if x == buscado => {
+                    return Some(meio);
+                }
+                x if x < buscado => inicio = meio + 1,
+                _ => fim = meio - 1,
+            }
         }
+        None
     }
-
-   match  encontrado {
-      true => Some("Encontrado"),
-      false => Some("Nao encontrado")
-   }
-  }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ use crate::find::busca_binaria;
 mod find;
 
 fn main() {
-    let busca = busca_binaria(&[0,2,4,6,8,10,12,14], 12).expect("Numero nao encontrado");
-    println!("{:?}", busca);
+    let array = [
+        2, 22, 48, 58, 61, 73, 84, 90, 100, 119, 132, 154, 160, 177, 187, 197, 201, 211, 2242,
+    ];
+
+    if let Some(busca) = busca_binaria(&array, 23){
+        println!("elemento 12 encontrado na posição {:?}", busca);
+    } else {
+        print!("elemento 12 não encontrado")
+    };
+    
+    
+    
 }
